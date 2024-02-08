6842dae4f5be6ae2e2c83afeb9d11805ed977b98
student <student@C313-3>
2024-02-08 18:13:27 +0300

    2задание

---
+++ src/2zadanie.kt
@@ -0,0 +1,28 @@
+/*
+fun main() {
+    var maxNumber = Int.MIN_VALUE
+    var input: String
+    do {
+        input = readLine() ?: ""
+        val number = input.toIntOrNull()
+        number?.plus(number)
+        if (number != null && number > maxNumber)
+            maxNumber = number
+    } while (input != "стоп")
+    println("$maxNumber")
+}*/
+
+
+fun main() {
+    val numbers = mutableListOf<Int>()
+    var input: String
+    do {
+        input = readLine() ?: ""
+        val number = input.toIntOrNull()
+        if (number != null) {
+            numbers.add(number)
+        }
+    } while (input != "стоп")
+    val maxNumber = numbers.maxOrNull()
+    println("$maxNumber")
+}
